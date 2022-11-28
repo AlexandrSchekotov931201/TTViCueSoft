802550940b6a278b9d91d80e52b35e8224bb5095
Alexandr Shchekotov <apshheko@mts.ru>
2022-11-28 21:40:45 +0600

    add floating text field

--- sources/feature/video-player-screen/src/main/java/ru/apshheko/video_player_screen/presentation/view/VideoPlayerScreenCompose.kt
+++ sources/feature/video-player-screen/src/main/java/ru/apshheko/video_player_screen/presentation/view/VideoPlayerScreenCompose.kt
@@ -2,17 +2,22 @@ package ru.apshheko.video_player_screen.presentation.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonColors
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.exoplayer2.ExoPlayer
@@ -24,6 +29,7 @@ import ru.apshheko.designsystem.Gray
 import ru.apshheko.designsystem.view.GlideImage
 import ru.apshheko.video_player_screen.presentation.model.Poster
 import ru.apshheko.video_player_screen.presentation.model.VideoPlayerScreenModel
+import kotlin.math.roundToInt
 
 @Composable
 fun VideoPlayerScreenView(
@@ -43,39 +49,74 @@ fun VideoPlayerScreenView(
 
 @Composable
 private fun VideoPlayer(fileUrl: String) {
-    AndroidView(
-        modifier = Modifier
-            .padding(start = 8.dp, end = 8.dp)
-            .fillMaxWidth()
-            .height(208.dp)
-            .clip(RoundedCornerShape(15.dp)),
-        factory = { context ->
-            StyledPlayerView(context).apply {
-                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
-                player = ExoPlayer.Builder(context).build()
-                player?.apply {
-                    repeatMode = Player.REPEAT_MODE_ONE
-                    useController = false
-                    addMediaItem(MediaItem.fromUri(fileUrl))
-                    prepare()
-                    play()
-                }
-            }
-        },
-        update = { view ->
-            view.apply {
-                player?.apply {
-                    stop()
-                    repeat(mediaItemCount) {
-                        removeMediaItem(it)
+    Box {
+        var sizeVideoPlayer: IntSize? = null
+        var sizeTextField: IntSize? = null
+        var textField by remember { mutableStateOf("")}
+        var offsetX by remember { mutableStateOf(0f) }
+        var offsetY by remember { mutableStateOf(0f) }
+
+        AndroidView(
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp)
+                .fillMaxWidth()
+                .height(208.dp)
+                .clip(RoundedCornerShape(15.dp))
+                .onGloballyPositioned {
+                    sizeVideoPlayer = it.size
+                },
+            factory = { context ->
+                StyledPlayerView(context).apply {
+                    resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
+                    player = ExoPlayer.Builder(context).build()
+                    player?.apply {
+                        repeatMode = Player.REPEAT_MODE_ONE
+                        useController = false
+                        addMediaItem(MediaItem.fromUri(fileUrl))
+                        prepare()
+                        play()
                     }
-                    addMediaItem(MediaItem.fromUri(fileUrl))
-                    prepare()
-                    play()
+                }
+            },
+            update = { view ->
+                view.apply {
+                    player?.apply {
+                        stop()
+                        repeat(mediaItemCount) {
+                            removeMediaItem(it)
+                        }
+                        addMediaItem(MediaItem.fromUri(fileUrl))
+                        prepare()
+                        play()
+                    }
                 }
             }
-        }
-    )
+        )
+        TextField(
+            modifier = Modifier
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        val maxYVideoPlayer = sizeVideoPlayer?.height ?: 0
+                        val maxXVideoPlayer = sizeVideoPlayer?.width ?: 0
+                        val maxYTextField = sizeTextField?.height ?: 0
+                        val maxXTextField = sizeTextField?.width ?: 0
+                        if ((offsetX + dragAmount.x) > 0 && (offsetX + dragAmount.x) < maxXVideoPlayer - maxXTextField) {
+                            offsetX += dragAmount.x
+                        }
+                        if ((offsetY + dragAmount.y) > 0 && (offsetY + dragAmount.y) < maxYVideoPlayer - maxYTextField) {
+                            offsetY += dragAmount.y
+                        }
+                    }
+                }
+                .onGloballyPositioned {
+                    sizeTextField = it.size
+                },
+            value = textField,
+            onValueChange = { textField = it}
+        )
+    }
 }
 
 @Composable
